3fb864ae75b5e23e78b1992f8b3a3ca5240f82d1
Artem Vorotnikov <artem@vorotnikov.me>
2020-11-06 13:50:29 +0300

    Additional trait bounds for keys

--- src/keys/mod.rs
+++ src/keys/mod.rs
@@ -35,11 +35,11 @@ use rlp::DecoderError;
 use std::{
     collections::BTreeMap,
     error::Error,
-    fmt::{self, Display},
+    fmt::{self, Debug, Display},
 };
 
 /// The trait required for a key to sign and modify an ENR record.
-pub trait EnrKey {
+pub trait EnrKey: Send + Sync + Unpin + 'static {
     type PublicKey: EnrPublicKey + Clone;
 
     /// Performs ENR-specific signing for the `v4` identity scheme.
@@ -58,7 +58,7 @@ pub trait EnrKey {
 }
 
 /// The trait required for a `PublicKey` to verify an ENR record.
-pub trait EnrPublicKey {
+pub trait EnrPublicKey: Clone + Debug + Send + Sync + Unpin + 'static {
     /// Verify an ENR signature for the `v4` identity scheme.
     fn verify_v4(&self, msg: &[u8], sig: &[u8]) -> bool;
 
